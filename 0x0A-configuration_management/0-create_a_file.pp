9a5b7dfaabdc7cede7d26599ef8305fb61f29226
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-04-21 19:06:12 -0700

    fixed arrow alignment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create file /tmp/school containing 'I love Puppet'
 file { '/tmp/school':
-ensure => file,
-mode   => '0744',
-owner  => 'www-data',
-group  => 'www-data',
+ensure  => file,
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
 content => 'I love Puppet',
 }
